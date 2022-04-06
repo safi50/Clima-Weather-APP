1ce26be199381a160a9bf809e7568909a85568ec
safi50 <63853856+safi50@users.noreply.github.com>
2022-04-06 23:10:53 +0500

    Add files via upload

---
+++ Clima Weather App/lib/services/location.dart
@@ -0,0 +1,43 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double long;
+  double lat;
+
+  Future<Position> determinePosition() async {
+    try {
+      bool serviceEnabled;
+      LocationPermission permission;
+
+      // Test if location services are enabled.
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
+      }
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        await Geolocator.openAppSettings();
+        await Geolocator.openLocationSettings();
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      // When we reach here, permissions are granted and we can
+      // continue accessing the position of the device.
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      lat = position.latitude;
+      long = position.longitude;
+      return position;
+    } catch (e) {
+      return e;
+    }
+  }
+}
